e48e24887d8cd0f327ec7203b00b6958c88a8db8
Rajankfl <kaflerams34@gmail.com>
2021-10-21 15:16:50 +0545

    bug fixed

--- dbCon.js
+++ dbCon.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const password= process.env.password;
 const connect = () => {
     //mongodb+srv://RajanKfl:MongoDBMERN@1stproj@cluster0.egfg1.mongodb.net/vsdata?retryWrites=true&w=majority
-    mongoose.connect(`mongodb+srv://RajanKfl:MongoDBMERN@1stproj@cluster0.egfg1.mongodb.net/vsdata?retryWrites=true&w=majority`, {
+    mongoose.connect(process.env.DATABASE, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
